ec2435b4b547b0e9de2664215f2c7914100150f1
Abhishek Kumar Singh <101357038+aksingh-002@users.noreply.github.com>
2022-11-23 06:25:49 +0530

    Update queen.py

--- queen.py
+++ queen.py
@@ -12,13 +12,11 @@ def isSafe(board, row, col):
 		if board[row][i] == 1:
 			return False
 
-	for i, j in zip(range(row, -1, -1),
-					range(col, -1, -1)):
+	for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
 		if board[i][j] == 1:
 			return False
 
-	for i, j in zip(range(row, N, 1),
-					range(col, -1, -1)):
+	for i, j in zip(range(row, N, 1), range(col, -1, -1)):
 		if board[i][j] == 1:
 			return False
 
@@ -29,15 +27,11 @@ def solveNQUtil(board, col):
 		return True
 
 	for i in range(N):
-
 		if isSafe(board, i, col):
 			board[i][col] = 1
-
 			if solveNQUtil(board, col + 1) == True:
 				return True
-
 			board[i][col] = 0
-
 	return False
 
 def solveNQ():
